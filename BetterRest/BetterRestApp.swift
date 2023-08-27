c96b9f1312887a5593af125198eccc77d667c59f
Marcio Curvello <marciocurvello@gmail.com>
2023-08-27 13:24:44 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Marcio Curvello on 27/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
